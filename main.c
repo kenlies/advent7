0d21b2f3ebea55836992630bedd9314101693e1d
kenlies <97135325+kenlies@users.noreply.github.com>
2022-12-08 01:54:32 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int	main(void)
 	total_count = 0;
 	tmp = 0;
 	count = 0;
-	fd_1 = open("input2.txt", O_RDONLY);
+	fd_1 = open("input.txt", O_RDONLY);
 
 	while (line = get_next_line(fd_1))
 	{	
@@ -31,27 +31,36 @@ int	main(void)
 		}
 		if (read_flag)
 		{
-			count += atoi(line);
+			count += (unsigned long)atoi(line);
 			if (count > 100000)
 			{
 				count = 0;
+				tmp = 0;
 				read_flag = 0;
+			}
+			else if ((count + tmp) > 100000)
+			{
+				count -= tmp;
 			}
 		}
 		if (line[0] == 'd')
 			dir_flag = 1;
-		if ((count + tmp) <= 100000 && dir_flag)
-			tmp = count;
-		else
-			tmp = 0;
 		if (line[0] == '$' && line[2] == 'c' && end_flag)
 		{
-			total_count += count;
-			
+			printf("%lu\n", count);
+			if ((count + tmp) <= 100000 && dir_flag)
+				tmp += count;
+
+			if (!dir_flag)
+			{
+				count += tmp * 2;
+				total_count += count;
+			}
 			dir_flag = 0;
 			end_flag = 0;
 			count = 0;
 		}
-		
 	}
+	//printf("%lu", tmp);
+	printf("%lu", total_count);
 }
